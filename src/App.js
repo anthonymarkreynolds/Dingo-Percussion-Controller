9d0c58f566786dec1031a70d23d77ef244ff6746
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2022-09-21 10:58:05 +1000

    add dials

--- src/App.js
+++ src/App.js
@@ -16,10 +16,30 @@ const Pads = () => {
   );
 };
 
+const Dial = ({ label, initValue }) => {
+  return (
+    <div className="dial-container">
+      <svg viewBox="0 0 100 100" style={{ maxHeight: 100 }}>
+        <circle
+          pathLength={1}
+          strokeDasharray={1}
+          strokeDashoffset={1 - initValue || 0.5}
+          className="dial"
+          cx="50"
+          cy="50"
+          r="40"
+        />
+      </svg>
+      {label && <h6 className="dial-label">{label}</h6>}
+    </div>
+  );
+};
+
 const PadControls = () => {
   return (
     <div className="pad-controls">
       <h3>Pad Controls</h3>
+      <Dial initValue={0.4} label="test" />
     </div>
   );
 };
@@ -31,6 +51,11 @@ const Sequencer = () => {
     </div>
   );
 };
+
+const Sample = () => {
+  <div className="sample"></div>;
+};
+
 function App() {
   return (
     <div className="layout">
