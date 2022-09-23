9fd66e1072bd5e4b8c2e538b0ef65c13a0943fe6
Todd0554 <toddy9229@outlook.com>
2022-09-23 11:19:34 +1000

    add day & night button

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import AudioCTX from "../audio/AudioCTX";
 import ToggleSwitch from "./ToggleSwitch";
+import ThemeStyleSwitch from "./ThemeStyleSwitch";
 
 const Explorer = () => {
   const { play } = useContext(AudioCTX);
@@ -8,6 +9,7 @@ const Explorer = () => {
     <div className="explorer">
       <h3>Sample Explorer</h3>
       <ToggleSwitch callback={(playing) => !playing && play()} />
+      <ThemeStyleSwitch />
     </div>
   );
 };
